0710b89e764265da0d391e8d62b850935e09bdc5
Jannath Jasmin <jannathjasmin01@gmail.com>
2024-03-30 20:52:01 +0530

    first commit

---
+++ day1a.py
@@ -0,0 +1,51 @@
+# 1
+my_dogs = 2
+print(my_dogs)
+
+# 2
+my_dogs = ['Sammy', 'Frankie']
+print(my_dogs)
+
+# 3
+a = 5
+print(a)
+
+#4. Here we assigned the integer object `5` to the variable name `a`.<br>Let's assign `a` to something else:
+a=10
+print(a)
+
+# 5.You can now use `a` in place of the number `10`:
+a=10
+b=a+a
+print(b)
+# 6.a=10
+print(a)
+
+# 7.Reassigning Variables
+a = 10
+a = a + 10
+print(a)  
+
+# 8.There's actually a shortcut for this. Python lets you add, subtract, multiply and divide numbers with reassignment using 
+a=20
+a+=10
+print(a)
+
+# 9 multiply
+a=30
+a*=2
+print(a)
+
+# 10
+a=10
+print(type(a))
+
+# 11
+a = (1,2)
+print(type(a))
+
+# 12
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes) 
